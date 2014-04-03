b8cfef3164d1b0072ba55f34b36f3d8113dcfefe
Ajay Vallabh <ajay.vallabh@gmail.com>
2014-04-02 21:49:17 -0400

    Itsa Mario

---
+++ pset1/mario.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+  int height;
+
+  do {
+    printf("Enter a height (1-23): \n");
+    height = GetInt();
+  } while (height < 1 || height > 23);
+
+  if (height == 0) {
+    return 0;
+  }
+
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < height-(i+1); j++) {
+      printf("%s", " ");
+    }
+    for (int k = 0; k < i+2; k++) {
+      printf("#");
+    }
+    printf("\n");
+  }
+}
