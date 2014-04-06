ba4f83c276c8665fc8614bb6ca6a5106c9ac8b33
Ajay Vallabh <ajay.vallabh@gmail.com>
2014-04-06 11:39:34 -0400

    Time for Change - Perl alternative

--- pset1/greedy.pl
+++ pset1/greedy.pl
@@ -44,13 +44,3 @@ sub main {
 }
 
 main();
-
-# for (my $i = 0; $i < $height; $i++) {
-#   for (my $j = 0; $j < $height-($i+1); $j++) {
-#     print " ";
-#   }
-#   for (my $k = 0; $k < $i+2; $k++) {
-#     print "#";
-#   }
-#   print "\n";
-# }
